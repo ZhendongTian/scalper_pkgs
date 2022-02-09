c2cd0b7d55fd5c8f6c5940c087fc452758b4669d
zhendongtian <zhendong.tian96@gmail.com>
2022-02-09 14:59:42 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,14 +20,6 @@ sudo dpkg -i kernel.deb
 
 sudo apt-get install -f -y
 
-wget https://github.com/ZhendongTian/scalper_pkgs/raw/main/file.tar.gz
-
-tar -xzf file.tar.gz
-
-cd scalper
-
-wget https://github.com/ZhendongTian/scalper_pkgs/raw/main/launch.sh
-
 conda init
 
 exec bash
