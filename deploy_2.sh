d6abf1cff8b980e548b72e83b52e701fcaec1713
Zhendong Tian <90920535+ZhendongTian@users.noreply.github.com>
2022-04-23 20:13:47 +0100

    Update deploy_2.sh

--- deploy_2.sh
+++ deploy_2.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 pip install requests==2.26 urllib3==1.26 pyro5 selenium undetected-chromedriver keyboard
+
+sudo wget https://github.com/ZhendongTian/scalper_pkgs/raw/main/dist.tar.gz
+
+sudo tar -xf dist.tar.gz
